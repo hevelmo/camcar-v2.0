4a7eb6fc618a2c0fcc5ed1b2113913cb89e40c47
Heriberto Velasco Mora <hevelmo060683@gmail.com>
2015-11-02 12:48:07 -0600

    delete content lib

--- resources/public/lib/run/core.run.this.js
+++
@@ -1,7 +0,0 @@
-/*
-	This file joins all the js create by the developer in an unique min js
-  	In order to use only this file in production.
-  	To do that it's necessary prepros application
- */
-
-//@prepros-append components/jquery-placeholder.js
