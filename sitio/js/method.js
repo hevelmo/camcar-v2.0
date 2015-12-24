fd695898e613c824719e9c035784025316bbc288
hevelmo <hevelmo060683@gmail.com>
2015-12-23 21:46:23 -0600

    commit

--- sitio/js/method.js
+++ sitio/js/method.js
@@ -1347,7 +1347,7 @@
         },
         recurrentSecionAgenciesPreowned: function() {
             dataStarSiteAgenciesPreownedAttributes = [
-                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content', 'style':'display: none;'}, '', 1],
+                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content'}, '', 1],
                 ['section', {'id':domEl._start_agencies_preowned_content_body, 'class':'large-pad text-hero-2 agencies-preowned about-content'}, '', 1],
                 ['section', {'id':domEl._start_agencies_preowned_small_screen, 'class':'large-pad about-content', 'style':'padding-top: 0px;'}, '', 1],
             ];
@@ -1683,7 +1683,7 @@
         },
         recurrentSecionInventoriesPreowned: function() {
             dataStarSiteInventoriesPreownedAttributes = [
-                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content', 'style':'display: none;'}, '', 1],
+                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content'}, '', 1],
                 ['div', {'id':domEl._start_inventories_preowned_action_bar, 'class':'about-content'}, '', 1],
                 ['div', {'id':domEl._start_body_content_main, 'class':'about-content'}, '', 1]
             ];
@@ -1918,27 +1918,12 @@
                 contactMethods_sem_premium_by_model.refreshForm(brandName, modelName, semId);
 
                 CAM.loadTemplate(tempsNames.recurrent_inventories_preowned_details_carousel_vehicles, domEl.div_recurrent_corousel_vehicles, semMarcaData);
-                owlCarouselMethods.owlCarousel();
-                $("#vehicle-slider").owlCarousel();
-                equalHeightsMethods.equalHeightsLoad();
-                viewSectionInventoriesPreownedMethodDetails.gridItemMediaquery();
-            }
-        },
-        gridItemMediaquery: function() {
-            mediaquery = window.matchMedia("screen and (min-width: 414px) and (max-width: 736px)");
-            if (mediaquery.matches) {
-                $('.owl-carousel .item').attr('style','margin-left: 55px; margin-right: 55px;');
-                $('.media-box').attr('style','height: 160px !important;');
-                console.log('mediaquery es min 414px');
-            } else {
-                $('.owl-carousel .item').attr('style','');
-                $('.media-box').attr('style','');
-                console.log('mediaquery no es min 414px');
+                equalHeightsMethods.equalHeightsLoad
             }
         },
         recurrentSecionInventoriesPreownedDetails: function() {
             dataStarSiteInventoriesPreownedAttributes = [
-                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content', 'style':'display: none;'}, '', 1],
+                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content'}, '', 1],
                 ['div', {'id':domEl._start_inventories_preowned_action_bar, 'class':'about-content'}, '', 1],
                 ['div', {'id':domEl._start_body_content_main, 'class':'about-content'}, '', 1]
             ];
@@ -2354,7 +2339,7 @@
         },
         recurrentSecionWorkShop: function() {
             dataStarSiteWorkShopAttributes = [
-                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content', 'style':'display: none;'}, '', 1],
+                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content'}, '', 1],
                 ['section', {'id':domEl._start_workshop_content_body, 'class':'large-pad text-hero-2 about-content'}, '', 1],
                 ['section', {'id':domEl._start_workshop_content_brand, 'class':'large-pad text-hero-2 about-content'}, '', 1],
                 ['div', {'id':domEl._start_workshop_content_agencies, 'class':'about-content', 'style':'background-color: #f9f9f9; padding-top: 20px; padding-bottom: 60px;'}, '', 1]
@@ -2401,7 +2386,7 @@
         },
         recurrentSecionRental: function() {
             dataStarSiteRentalAttributes = [
-                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content', 'style':'display: none;'}, '', 1],
+                ['div', {'id':domEl._start_utility_bar_breadcrumb, 'class':'about-content'}, '', 1],
                 ['section', {'id':domEl._start_rental_content_body, 'class':'large-pad text-hero-2 about-content'}, '', 1],
                 ['div', {'id':domEl._start_rental_content_agencies, 'class':'about-content', 'style':'background-color: #f9f9f9; padding-top: 20px; padding-bottom: 60px;'}, '', 1]
             ];
@@ -2796,7 +2781,7 @@
 
             $('body,html').animate({ scrollTop: "0" }, 999, 'easeOutExpo' );
 
-            //console.log($element);
+            console.log($element);
             Finch.navigate('/seminuevos/inventarios/' + $nameBrand + '/' + $nameModel + '/' + $semId);
         },
         clickGo_workshop: function(event) {
@@ -3382,22 +3367,7 @@
 \* ------------------------------------------------------ */
     var inputValMetdods = {
         isIntegerKP: function (event) {
-            var key, numeros, teclado, especiales, teclado_especial, i;
-
-            key = event.keyCode || event.which;
-            teclado = String.fromCharCode(key);
-            numeros = '0123456789';
-            especiales = [8,9,37,38,39,40,46]; // array
-            teclado_especial = false;
-
-            for ( i in especiales ) {
-                if ( key == especiales[i] ) {
-                    teclado_especial = true;
-                }
-            }
-            if ( numeros.indexOf(teclado) == -1 && !teclado_especial ) {
-                return false;
-            }
+            return /\d/.test(String.fromCharCode(event.keyCode));
         },
         //http://www.lawebdelprogramador.com/foros/JavaScript/1074741-introducir_solo_numero_dos_decimales.html
         isDecimalKP: function(event) {
